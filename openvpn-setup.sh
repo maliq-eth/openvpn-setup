3b062071c3a3f4e5014d43bc1fb05eb4d0e7d7e5
maliq.eth <95887023+maliq-eth@users.noreply.github.com>
2025-04-12 16:42:51 +0700

    Update openvpn-setup.sh

--- openvpn-setup.sh
+++ openvpn-setup.sh
@@ -82,6 +82,16 @@ function get_ipv4_options() {
   mapfile -t ip_list < <(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1)
 
   if [[ ${#ip_list[@]} -eq 0 ]]; then
+    ip_list[0]=$(curl -4 -s ifconfig.me)
+  fi
+
+  printf "%s\n" "${ip_list[@]}"
+}
+
+function get_ipv4_options() {
+  mapfile -t ip_list < <(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1)
+
+  if [[ ${#ip_list[@]} -eq 0 ]]; then
     echo "Tidak ada IP lokal yang ditemukan. Mencoba mendapatkan IP publik..."
     ip_list[0]=$(curl -4 -s ifconfig.me || echo "Tidak ada IP")
   fi
@@ -92,30 +102,21 @@ function get_ipv4_options() {
 
 function select_ip() {
   local ips=()
-  local selected_ip
   while IFS= read -r line; do
     ips+=("$line")
   done < <(get_ipv4_options)
 
-  if [[ ${#ips[@]} -eq 0 || ${ips[0]} == "Tidak ada IP" ]]; then
-    echo "Gagal mendapatkan IP publik atau lokal. Pastikan server memiliki koneksi internet dan IP yang valid."
-    exit 1
-  fi
-
   if [[ ${#ips[@]} -eq 1 ]]; then
-    selected_ip="${ips[0]}"
-    echo "$selected_ip"
+    echo "${ips[0]}"
   else
     echo -e "\nPilih IP publik yang akan digunakan untuk klien:"
     for i in "${!ips[@]}"; do
-      echo "$((i + 1)). ${ips[$i]}"
+      echo "$((i+1)). ${ips[$i]}"
     done
-
     while true; do
       read -rp "Pilih [1-${#ips[@]}]: " ip_choice
       if [[ $ip_choice =~ ^[0-9]+$ ]] && ((ip_choice >= 1 && ip_choice <= ${#ips[@]})); then
-        selected_ip="${ips[$((ip_choice - 1))]}"
-        echo "$selected_ip"
+        echo "${ips[$((ip_choice-1))]}"
         return
       else
         echo "Pilihan tidak valid, coba lagi."
@@ -128,11 +129,6 @@ function create_client() {
   echo -n "Masukkan nama client: "
   read -r unsanitized_client
   client=$(sed 's/[^a-zA-Z0-9_-]/_/g' <<< "$unsanitized_client")
-
-  if [[ -z $client ]]; then
-    echo "Nama client tidak boleh kosong atau hanya berisi karakter yang difilter."
-    return
-  fi
 
   cd ~/openvpn-ca/easy-rsa/easyrsa3 || exit
   ./easyrsa build-client-full "$client" nopass
@@ -169,3 +165,59 @@ function create_client() {
   echo -e "${green}Client config $client.ovpn berhasil dibuat di ~/client-configs/files/${nc}"
   pause
 }
+
+function list_clients() {
+  echo -e "${green}Daftar Client:${nc}"
+  ls ~/openvpn-ca/easy-rsa/easyrsa3/pki/issued/
+  pause
+}
+
+function remove_client() {
+  echo -n "Masukkan nama client yang akan dihapus: "
+  read -r client
+  cd ~/openvpn-ca/easy-rsa/easyrsa3 || exit
+  ./easyrsa revoke "$client"
+  ./easyrsa gen-crl
+  cp pki/crl.pem /etc/openvpn/crl.pem
+  rm -f ~/client-configs/files/"$client".ovpn
+  echo -e "${green}Client $client telah dihapus.${nc}"
+  pause
+}
+
+function uninstall_openvpn() {
+  echo -e "${red}WARNING: Ini akan menghapus OpenVPN dan semua konfigurasinya.${nc}"
+  read -rp "Ketik 'ya' untuk melanjutkan: " confirm
+  if [[ "$confirm" == "ya" ]]; then
+    systemctl stop openvpn@server
+    apt remove --purge -y openvpn easy-rsa
+    rm -rf /etc/openvpn ~/openvpn-ca ~/client-configs
+    echo -e "${red}OpenVPN telah dihapus.${nc}"
+  fi
+  pause
+}
+
+function main_menu() {
+  while true; do
+    clear
+    echo -e "${green}========= OpenVPN Menu =========${nc}"
+    echo "1. Install OpenVPN"
+    echo "2. Tambah Client"
+    echo "3. Hapus Client"
+    echo "4. Lihat Daftar Client"
+    echo "5. Uninstall OpenVPN"
+    echo "6. Keluar"
+    echo "================================="
+    read -rp "Pilih menu [1-6]: " menu
+    case $menu in
+      1) install_openvpn ;;
+      2) create_client ;;
+      3) remove_client ;;
+      4) list_clients ;;
+      5) uninstall_openvpn ;;
+      6) exit 0 ;;
+      *) echo "Pilihan tidak valid!" ; pause ;;
+    esac
+  done
+}
+
+main_menu
